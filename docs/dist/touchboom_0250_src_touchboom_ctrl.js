1d563edf61486684b5a87a6b8b884ab61bbb3323
chris <chris@bumblehead.com>
2023-10-26 21:58:41 -0700

    added files for github-pages

---
+++ docs/dist/touchboom_0250_src_touchboom_ctrl.js
@@ -0,0 +1,347 @@
+// Filename: touchboom_ctrl.js
+// Timestamp: 2018.01.21-20:41:01 (last modified)
+// Author(s): bumblehead <chris@bumblehead.com>
+//
+// http://ariya.github.io/kinetic/
+// https://ariya.io/2011/10/flick-list-with-its-momentum-scrolling-and-deceleration
+//
+// top left mouse event is position 0,0
+//
+// +---------------------->|
+// | (0, 0)                | (10, 0)
+// |                       |
+// |                       |
+// |                       |
+// |                       |
+// |                       |
+// |                       |
+// v (0, 10)               | (10, 10)
+// ------------------------+
+//
+
+import touchboom_ev from './touchboom_0250_src_touchboom_ev.js'
+import curved from './curved_008_curved.js'
+
+export default (o => {
+  const VELOCITYTHRESHOLD = 10
+  const TIMECONST = 325
+  const INF = Infinity;
+
+  o = (cfg, parentelem, fn, onmovefn, onoverfn) => {
+    cfg.publishfn = fn;
+    cfg.onmovefn = onmovefn || function () {};
+    cfg.onoverfn = onoverfn || function () {};
+    cfg.isinertia = typeof cfg.isinertia === 'boolean' ? cfg.isinertia : true;
+    cfg.rafcoordidarr = [];
+    cfg = o.coordsreset(cfg);
+
+    return cfg;
+  };
+
+  o.detach = cfg => {
+    cfg.publishfn = null;
+    cfg.onmovefn = null;
+    cfg.onoverfn = null;
+    cfg.rafcoordidarr = [];
+
+    return cfg;
+  };
+
+  o.events = {
+    INTERRUPT : 'interrupt',
+    CANCEL : 'cancel',
+    KEYSTART : 'keystart',
+    KEYEND : 'keyend',
+    MOVE : 'move',
+    MOVEEND : 'moveend',
+    OVER : 'over',
+    START : 'start',
+    END : 'end',
+    TAP : 'tap',
+    OUT : 'out', // mouse leaves area
+    TAPTAP : 'taptap'
+  };
+
+  o.onmoveend = (cfg, id, fn) => (
+    cfg.onmoveendfn = cfg.onmoveendfn || {},
+    cfg.onmoveendfn[id] = fn,
+    cfg);
+
+  o.onmoveendfn = (cfg, type, ev) => {
+    if (cfg.onmoveendfn) {
+      Object.keys(cfg.onmoveendfn).map(key => {
+        cfg.onmoveendfn[key](cfg, type, ev);
+      });
+    }
+
+    cfg = touchboom_ev.publish(cfg, o.events.MOVEEND, cfg.laste);
+
+    return cfg;
+  };
+
+  o.onmovefn = (cfg, e) => {
+    try {
+      cfg.onmovefn(cfg, e);
+    } catch (e) {
+      o.stop(cfg);
+      console.error('[!!!] onmove', e);
+    }
+  };
+
+  o.coords = (cfgcoords = [ {}, {} ]) =>
+    cfgcoords.map(o.coordget);
+
+  o.coordsreset = cfg => (
+    // initialize empty coords, two coords by default --x and y
+    cfg.coords = o.coords(cfg.coords),
+    cfg);
+
+  o.isnum = n =>
+    typeof n === 'number';
+
+  o.firstnum = numarr =>
+    numarr.find(num => typeof num === 'number');
+
+  o.coordget = (c = {}, coorddef = {}) => {
+    const now = Date.now();
+
+    c.min = o.firstnum([ c.min, coorddef.min, -INF ]);
+    c.max = o.firstnum([ c.max, coorddef.max, INF ]);
+
+    // allow changes outside controls to affect ismin and ismax behaviour
+    c.getmin = c.getmin || ((cfg, c) => c.min);
+    c.getmax = c.getmax || ((cfg, c) => c.max);
+
+    c.trackts = now;
+
+    // glide coordinates are auto-updated with fixed target and amplitude
+    // non-glide coordinates continually updated w/ new target and amplitude
+    // isglide defined w/ timestamp of glide initialisation
+    c.isglide = Boolean(coorddef.isglide) && now;
+
+    // ismove is timestamp of move initialiszation
+    c.ismove = Boolean(coorddef.ismove) && now;
+    c.startpos = +coorddef.start;
+    c.lastpos = +coorddef.start;
+
+    if (o.isnum(c.total)) {
+      // do nothing
+    } else if (o.isnum(c.bgn)) {
+      c.total = c.bgn;
+    } else if (o.isnum(coorddef.bgn)) {
+      c.total = coorddef.bgn;
+    } else {
+      c.total = 0;
+    }
+
+    c.autoweight = c.autoweight || coorddef.autoweight;
+    c.offset = 0;
+    c.target = 0;
+    c.velocity = 0;
+    c.amplitude = 0;
+    c.frame = 0;
+    c.offset = 0;
+    c.type = coorddef.type || c.type;
+
+    return c;
+  };
+
+  o.coordgetstarted = (c, start, type) =>
+    o.coordget(c, { start, type });
+
+  o.coordset = (c, props) =>
+    Object.assign({}, c, props);
+
+  // mutates cfg.coords
+  o.coordsupdatepassive = (cfg, now = Date.now()) => (
+    cfg.coords = cfg.coords.map(c => (
+      c.isupdatepassive ? o.coordupdatepassive(cfg, c, now) : c)),
+    cfg
+  );
+
+  o.coordsgettotal = cfg => (
+    o.coordsupdatepassive(cfg).coords.map(c => c.total + c.offset));
+
+  o.coordsismove = cfg =>
+    cfg.coords.some(c => c.ismove);
+
+  o.stop = cfg => (
+    cfg.ticker = clearInterval(cfg.ticker),
+    cfg);
+
+  o.start = (cfg, e) => (
+    cfg.rafcoordidarr = cfg.coords.map((c, i) => (
+      c.ismove || cfg.rafcoordidarr[i])),
+    cfg.ticker = setInterval(() => o.coordsupdate(cfg), 100),
+    o.coordsmovestart(cfg, e),
+    cfg);
+
+  o.getdeltacurve = ((maxms, curve) => {
+    maxms = 1000;
+    curve = curved(3, 10, 'bgn');
+
+    return (c, timestart, timenow) => {
+      const timediff = (timenow - timestart) / maxms;
+
+      return (c.autoweight
+        ? curved(c.autoweight * 0.2, c.autoweight, 'bgn')
+        : curve)(timediff > 1 ? 1 : timediff);
+    };
+  })();
+
+  o.getvelocityupdated = (offset, frame, velocity, elapsedms) => (
+    0.4 * ( // 0.8 * (
+      1000 * ( // velocity
+        offset - frame // delta
+      ) / (1 + elapsedms)) + 0.2 * velocity
+  );
+
+  o.getamplitudeupdated = (velocity, amplitude, velocitythreshold) =>
+    Math.abs(velocity) > velocitythreshold ? 0.8 * velocity : amplitude;
+
+  o.gettargetupdated = (velocity, offset, amplitude) =>
+    Math.abs(velocity) > 10 ? Math.round(offset + amplitude) : offset;
+
+  o.coordupdatepassive = (cfg, c, now = Date.now()) => {
+    if (c.isupdatepassive) {
+      c.lastdelta = c.lastdelta || 0;
+      c.lastdelta += o.getdeltacurve(c, c.ismove, now) * c.dir;
+      c = o.updatecoord(cfg, c, c.lastdelta);
+    }
+
+    return c;
+  };
+
+  o.coordupdate = (cfg, c, date = Date.now()) =>
+    o.coordset(c, {
+      velocity : o.getvelocityupdated(c.offset, c.frame, c.velocity, date - c.trackts),
+      frame : c.offset,
+      trackts : date
+    });
+
+  o.coordsupdate = (cfg, now = Date.now()) => {
+    cfg.coords = cfg.coords.map(c => (
+      c.isglide ? c : o.coordupdate(cfg, c, now)
+    ));
+
+    return cfg;
+  };
+
+  // offset, cur, max,  min
+  // 40,     100,   0,    100   => 0
+  // 40,     80,    0,    100   => 20
+  // 40,     20,    0,    100   => 40
+  // 3,      -100,  -100, 100   => 0
+  // -491,   0,     -100, 100   => -100
+  o.getoffset = (cfg, offset, cur, min, max) => (
+    offset > 0
+      ? ((cur + offset <= max)
+        ? offset : cur >= max ? 0 : max - cur)
+      : ((cur + offset >= min)
+        ? offset : cur <= min ? 0 : min - cur));
+
+  o.movecoord = (cfg, c, dist) =>
+    o.coordset(c, {
+      offset : o.getoffset(cfg, dist, c.total, c.getmin(cfg, c), c.getmax(cfg, c))
+    });
+
+  o.movecoordauto = (cfg, c, now) => {
+    const elapsed = now - c.trackts;
+    const delta = -c.amplitude * Math.exp(-elapsed / TIMECONST);
+
+    if (Math.abs(delta) > 0.5) {
+      c = o.movecoord(cfg, c, c.target + delta);
+    } else {
+      c.ismove = false;
+      c.isglide = false;
+      c.total += c.offset;
+      c.offset = 0;
+    }
+
+    return c;
+  };
+
+  o.automove = (cfg, e, rafcoordidarr, now = Date.now()) => {
+    // continue moving only if an axis moved here is not begun elswhere
+    // (indicated by newer/different rafcoordid)
+    //
+    if (!rafcoordidarr.some((coordid, i) => (
+      coordid && coordid === cfg.rafcoordidarr[i]
+    ))) {
+      return 'automove cancelled' && null;
+    }
+
+    cfg.coords = cfg.coords.map(c => (
+      c.isglide ? c = o.movecoordauto(cfg, c, now) : c));
+
+    if (o.coordsismove(cfg)) {
+      requestAnimationFrame(() => {
+        o.onmovefn(cfg, e);
+        o.automove(cfg, e, rafcoordidarr);
+      });
+    } else {
+      cfg = o.onmoveendfn(cfg, 'moveend', e);
+    }
+  };
+
+  o.updatecoord = (cfg, c, pos, delta) => {
+    delta = c.lastpos - pos;
+
+    if (c.ismove) {
+      if (!c.isinertia || Math.abs(delta) > 2) {
+        c.lastpos = pos;
+        c = o.movecoord(cfg, c, c.offset - delta);
+      }
+    }
+
+    return c;
+  };
+
+  o.coordstopped = (cfg, c) => {
+    c.total += c.offset;
+    c.offset = 0;
+    c.ismove = false;
+
+    return c;
+  };
+
+  o.coordmoveend = (cfg, c, now = Date.now()) => {
+    if (Math.abs(c.velocity) > VELOCITYTHRESHOLD) {
+      c.trackts = now;
+
+      c.amplitude = o.getamplitudeupdated(
+        c.velocity, c.amplitude, VELOCITYTHRESHOLD);
+
+      c.target = o.gettargetupdated(
+        c.velocity, c.offset, c.amplitude, c.target);
+
+      c.isglide = Date.now();
+    } else {
+      c = o.coordstopped(cfg, c);
+    }
+
+    return c;
+  };
+
+  o.coordsmovestart = (cfg, e) =>
+    requestAnimationFrame(() => {
+      if (cfg.ticker) {
+        // ticker is active while dragging
+        o.coordsmovestart(cfg, e);
+        o.onmovefn(cfg, e);
+      }
+    });
+
+  o.coordsmoveend = (cfg, e) => {
+    cfg = o.stop(cfg);
+
+    if (cfg.isinertia && o.coordsismove(cfg)) {
+      requestAnimationFrame(() => o.automove(cfg, e, cfg.rafcoordidarr.slice()));
+    } else {
+      cfg.coords = cfg.coords.map(c => o.coordstopped(cfg, c));
+      cfg = o.onmoveendfn(cfg, 'moveend', e);
+    }
+  };
+
+  return o;
+})({});
---
+++ docs/dist/touchboom_0250_src_touchboom_key.js
@@ -0,0 +1,192 @@
+// Filename: touchboom_key.js
+// Timestamp: 2018.01.15-15:05:24 (last modified)
+// Author(s): bumblehead <chris@bumblehead.com>
+
+import evdelegate from './evdelegate_004_evdelegate.js'
+import nodefocusable from './nodefocusable_002_nodefocusable.js'
+
+import touchboom_ev from './touchboom_0250_src_touchboom_ev.js'
+import touchboom_ctrl from './touchboom_0250_src_touchboom_ctrl.js'
+
+export default (o => {
+  const DIR_LEFT = 'DIR_LEFT';
+  const DIR_RIGHT = 'DIR_RIGHT';
+  const DIR_DOWN = 'DIR_DOWN';
+  const DIR_UP = 'DIR_UP';
+  const {
+    INTERRUPT,
+    KEYSTART, KEYEND
+  } = touchboom_ctrl.events;
+
+  o = (cfg, parentelem, fn) =>
+    o.connectdelegate(cfg, parentelem, fn);
+
+  o.events = {
+    DIR_LEFT, DIR_RIGHT, DIR_DOWN, DIR_UP
+  };
+
+  o.getdirection = (cfg, keyCode) => {
+    let dir = null;
+
+    if (typeof keyCode !== 'number')
+      throw new Error('keyCode must be a number');
+    else if (cfg.iskeyupre.test(keyCode))
+      dir = DIR_UP;
+    else if (cfg.iskeyleftre.test(keyCode))
+      dir = DIR_LEFT;
+    else if (cfg.iskeydownre.test(keyCode))
+      dir = DIR_DOWN;
+    else if (cfg.iskeyrightre.test(keyCode))
+      dir = DIR_RIGHT;
+
+    return dir;
+  };
+
+  // LEFT OR RIGHT, X: 0
+  // UP or DOWN,    Y: 1
+  o.getdirectionxynum = (cfg, dir) =>
+    Number(dir === DIR_DOWN || dir === DIR_UP);
+
+  // LEFT OR DOWN: -1
+  // UP or RIGHT:  +1
+  o.getdirectiondirnum = (cfg, dir) =>
+    Number(dir === DIR_RIGHT || dir === DIR_DOWN) ||
+    -Number(dir === DIR_LEFT || dir === DIR_UP);
+
+  o.keydown = (cfg, touchboom_ctrl, e) => {
+    let dir = o.getdirection(cfg, e.keyCode),
+        axisnum = o.getdirectionxynum(cfg, dir),
+        dirnum = o.getdirectiondirnum(cfg, dir),
+        coord = cfg.coords[axisnum];
+
+    e.lastkeye = e;
+    if (coord && !coord.isupdatepassive) {
+      if (coord.ismove) {
+        coord = touchboom_ctrl.coordset(coord, {
+          total : coord.total + coord.offset,
+          offset : 0
+        });
+
+        cfg.coords[axisnum] = coord;
+        cfg = touchboom_ev.publish(cfg, INTERRUPT, e);
+      }
+      coord.isupdatepassive = true;
+      coord = touchboom_ctrl.coordget(coord, {
+        ismove : Date.now(),
+        start : 0,
+        type : 'key'
+      });
+      coord.lastdelta = 0;
+      coord.dir = dirnum;
+      cfg.coords[axisnum] = coord;
+
+      cfg = touchboom_ev.publish(cfg, KEYSTART, e);
+      touchboom_ctrl.start(cfg, e);
+    }
+
+    // prevent key from scrolling parent|document
+    if (cfg.isKeyPreventDefault)
+      e.preventDefault();
+  };
+
+  o.keyup = (cfg, touchboom_ctrl, e) => {
+    let dir = o.getdirection(cfg, e.keyCode),
+        axisnum = o.getdirectionxynum(cfg, dir),
+        coord = cfg.coords[axisnum];
+
+    if (coord && coord.isupdatepassive) {
+      coord.isupdatepassive = false;
+      cfg = touchboom_ctrl.stop(cfg);
+      cfg.coords[axisnum] = touchboom_ctrl.coordmoveend(cfg, coord);
+      touchboom_ctrl.coordsmoveend(cfg, e, coord);
+
+      cfg = touchboom_ev.publish(cfg, KEYEND, e || cfg.lastkeye);
+    }
+  };
+
+  o.getkeycodearrre = keycodearr =>
+    new RegExp(keycodearr.join('|'));
+
+  o.adaptkeycodere = cfg => (
+    cfg.iskeyupre = o.getkeycodearrre(
+      cfg.keycodeuparr || [ 38, 87 ]),
+    cfg.iskeyleftre = o.getkeycodearrre(
+      cfg.keycodeleftarr || [ 37, 65 ]),
+    cfg.iskeyrightre = o.getkeycodearrre(
+      cfg.keycoderightarr || [ 39, 68 ]),
+    cfg.iskeydownre = o.getkeycodearrre(
+      cfg.keycodedownarr || [ 40, 83 ]),
+    cfg);
+
+  o.connect = (cfg, touchboom, parentelem) => {
+    cfg = o.adaptkeycodere(cfg);
+
+    touchboom_ev.lsnpub(cfg, parentelem, [ 'keydown' ], (cfg, e) => {
+      o.keydown(cfg, touchboom, e);
+    });
+
+    touchboom_ev.lsnpub(cfg, parentelem, [ 'keyup' ], (cfg, e) => {
+      o.keyup(cfg, touchboom, e);
+    });
+
+    return cfg;
+  };
+
+  o.delegator = null;
+
+  o.detach = (cfg, parentelem) => {
+    o.delegator = evdelegate.rmelemstate(o.delegator, parentelem);
+
+    return cfg;
+  };
+
+  o.reset = (cfg, elem) => {
+    o.delegator = evdelegate.addelemstate(o.delegator, elem, cfg);
+
+    return cfg;
+  };
+
+  o.connectdelegate = (cfg, touchboom, parentelem) => {
+    let { body } = document
+
+    if (!parentelem || !parentelem.id) {
+      console.error('parent element exist w/ valid id');
+      return cfg;
+    }
+
+    if (!o.delegator) {
+      o.delegator = evdelegate.create();
+
+      evdelegate.lsnpubarr(o.delegator, {}, body, [ 'keydown' ], (cfg, e) => {
+        const elem = e.target;
+        const delegatorstate = evdelegate.getelemstate(
+          o.delegator, nodefocusable(elem));
+
+        if (delegatorstate) {
+          o.delegator = evdelegate.setactivestate(o.delegator, delegatorstate);
+          o.keydown(evdelegate.getstatemeta(delegatorstate), touchboom, e);
+        }
+      });
+
+      evdelegate.lsnpubarr(o.delegator, {}, body, [ 'keyup' ], (cfg, e) => {
+        let delegatorstate = evdelegate.getactivestate(o.delegator);
+
+        if (delegatorstate) {
+          o.keyup(evdelegate.getstatemeta(delegatorstate), touchboom, e);
+        }
+      });
+    }
+
+    cfg = touchboom_ctrl.onmoveend(cfg, 'key', (/* cfg, type, e */) => {
+      evdelegate.rmactivestate(o.delegator);
+    });
+
+    cfg = o.adaptkeycodere(cfg);
+
+    o.delegator = evdelegate.addelemstate(o.delegator, parentelem, cfg);
+
+    return cfg;
+  };
+
+  return o;
+})({});
